a83dfb36134f137fd87fe6af3f5dce6f8b9580d5
Kylan Larson <kylan.larson@southeasttech.edu>
2023-09-27 15:08:07 -0500

    Add project files.

---
+++ Ch04Ex1MovieList/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ch04Ex1MovieList.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
