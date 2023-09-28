20165c7f11ce64e2efb922c04cd9f24ebae8cb11
Kylan Larson <kylan.larson@southeasttech.edu>
2023-09-28 13:06:32 -0500

    did step 12

---
+++ Ch04Ex1MovieList/Models/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Ch04Ex1MovieList.Models;
+
+namespace Ch04Ex1MovieList.Models
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<MovieContext>(options =>
+                    options.UseSqlServer(
+                            Configuration.GetConnectionString("MovieContext")));
+        }
+    }
+}
