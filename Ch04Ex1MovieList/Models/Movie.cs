01551796d26206987aa05de12c40c82ca36438e7
Kylan Larson <kylan.larson@southeasttech.edu>
2023-09-29 10:14:27 -0500

    did 37

--- Ch04Ex1MovieList/Models/Movie.cs
+++ Ch04Ex1MovieList/Models/Movie.cs
@@ -18,8 +18,10 @@ namespace Ch04Ex1MovieList.Models
 
         [Required(ErrorMessage = "Please enter a genre")]
         public string? GenreId { get; set; }
-        public Genre? Genre { get; set; }    
+        public Genre? Genre { get; set; }
 
+        public string Slug =>
+            Name?.Replace(' ', '-').ToLower() + '-' + Year?.ToString();
 
     }
 }
--- Ch04Ex1MovieList/Models/Startup.cs
+++ Ch04Ex1MovieList/Models/Startup.cs
@@ -5,22 +5,51 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Ch04Ex1MovieList.Models;
 
-namespace Ch04Ex1MovieList.Models
+namespace Ch04Ex1MovieList
 {
     public class Startup
     {
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-            
         }
+
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<MovieContext>(options =>
-                    options.UseSqlServer(
-                            Configuration.GetConnectionString("MovieContext")));
+                options.UseSqlServer(Configuration.GetConnectionString("MovieContext")));
+            services.AddRouting(options =>
+            {
+                options.LowercaseUrls = true;
+                options.AppendTrailingSlash = true;
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}/{slug?}");
+            });
         }
     }
 }
